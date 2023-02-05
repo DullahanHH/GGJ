16d5be14ad26a7824a0924cdc96e2769d9ebbe2c
DullahanHH <hunterzhou2000@gmail.com>
2023-02-05 03:29:15 -0400

    bind square buttons

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -21,6 +21,7 @@ public class MainMenu : MonoBehaviour
     }
 
     public void Chatper1_0(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("Intro");
     }
 
@@ -30,28 +31,35 @@ public class MainMenu : MonoBehaviour
     }
 
     public void Chatper1_2(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("1-2");
     }
 
     public void Chatper1_3(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("1-3");
     }
 
     public void Chatper1_4(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("1-4");
     }
 
     public void Chatper2_1(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("2-1");
     }
 
     public void Chatper2_2(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("2-2");
     }
     public void Chatper3_1(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("3-1");
     }
     public void Chatper3_2(){
+        FindObjectOfType<BGMManager>().ReplaceSound("level");
         SceneManager.LoadScene("3-2");
     }
   public void reloadScene(){
