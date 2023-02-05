c0ebfa48b4f99d6c6e069da453ef0f37c9b87f57
Joj-oker <64368425+Joj-oker@users.noreply.github.com>
2023-02-05 13:56:21 -0400

    update

---
+++ Assets/levelLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Dialog_logic.isFinalDialogFinished) {
+            LoadNextLevel();
+            Debug.Log("asdadasgfa");
+        }
+    }
+
+
+    // Update is called once per frame
+public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+IEnumerator LoadLevel(int levelIndex) {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
+
